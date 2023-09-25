8cd2de242b3da207869a939b6908d0afc41d8f35
frank <franklinokrah465@gmail.com>
2023-09-25 22:59:06 +0000

    Your commit message here

---
+++ lib/api/result_model.dart
@@ -0,0 +1,44 @@
+class ResultModel {
+  final String id;
+  final String name;
+  final String color;
+  final String numberPlate;
+  final String ownerFullName;
+  final String registeredAt;
+  final String driversImage;
+  final String carsImage;
+  final String ownerGender;
+  final String ownersNationality;
+  final String ownersAge;
+  final String registrationExpiry;
+  final String vehicleCategory;
+  final String chasisNumber;
+  final String nationalIdentification;
+  final String vehicleBrand;
+  final bool  registered;
+  final String model;
+
+
+  ResultModel({
+    required this.color,
+    required this.id,
+    required this.name,
+    required this.numberPlate,
+    required this.ownerFullName,
+    required this.registeredAt,
+    required this.driversImage,
+    required this.carsImage,
+    required this.ownerGender,
+    required this.ownersNationality,
+    required this.ownersAge,
+    required this.registrationExpiry,
+    required this.vehicleCategory,
+    required this.chasisNumber,
+    required this.nationalIdentification,
+    required this.vehicleBrand,
+    required this.registered,
+    required this.model,
+  });
+}
+
+
